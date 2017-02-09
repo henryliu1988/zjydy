16c9072fdd61328586465fadfeff6f4ca8a1cf8a
henryliu1988 <lytao123sc@126.com>
2017-02-09 13:58:20 +0800

    dd

--- app/src/main/java/com/zhjydy/model/net/WebKey.java
+++ app/src/main/java/com/zhjydy/model/net/WebKey.java
@@ -14,6 +14,7 @@ public class WebKey {
     public static final String WEBKEY_BASE = "http://59.110.24.36/yiduyi/";
     public static final String WEBKEY_URL_RES = "http://59.110.24.36/yiduyi/index.php?s=App/Common/imgUpload";
 
+    public static final String TYPE_KEY = "1";
 
     public static final int func_login = 1001;   //登录
     public static final int func_checkMobile = 1002;  //检查手机号是否被注册
